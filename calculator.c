c362b2a75266d59a7123bbedb0739805ce3a3d6e
Makayla Coomer <makayla.coomer@outlook.com>
2023-12-15 15:56:46 -0600

    Added comments and documentation

--- calculator.c
+++ calculator.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+
+//func proto
 double add(double a, double b);
 double sub(double a, double b);
 double mul(double a, double b);
@@ -7,12 +9,16 @@ double div(double a, double b);
 int main(){
     char choice;
     double a,b,ans;
+
+    //get user input for numbers and opperations
     printf("Would you like to add(a), subtract(s), multiply(m), or divide(d): \n");
     scanf("%c" , &choice);
     printf("Enter two numbers: \n");
     scanf("%lf" , &a);
     scanf("%lf" , &b);
     
+
+    // use of switch statments to determine function call
     switch (choice){
         case 'a':
             ans = add(a,b);
@@ -25,14 +31,14 @@ int main(){
             break;
         case 'd':
             if(b==0){
-                printf("Cannot divide by 0");
+                printf("Cannot divide by 0"); //edge case handling
                 return 0;
                 break;
             }else
             ans = div(a,b);
             break;
         default:
-            printf("Invalid opperation");
+            printf("Invalid opperation"); //edge case handling
     }
 
 
