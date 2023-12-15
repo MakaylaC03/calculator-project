4aa6b24bf78116c65bbb068bffb42d12fcc7cd3e
Makayla Coomer <makayla.coomer@outlook.com>
2023-12-15 15:33:45 -0600

    Implemented addition feature

--- calculator.c
+++ calculator.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
+double add(double a, double b);
 
 int main(){
+    double a,b,ans;
+    printf("Enter two numbers to add: \n");
+    scanf("%lf" , &a);
+    scanf("%lf" , &b);
+    ans= add(a,b);
+    printf("%lf", ans);
+    return 0;
+    
 
+}
+
+double add(double a, double b){
+    return(a+b);
 }
